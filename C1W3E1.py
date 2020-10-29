2020c8985f1af00e60a34b5f7226d1f4b54ecb40
FiliVanDev <FiliVanDev@gmail.com>
2020-10-29 15:50:17 +1200

    Add Exercise 1 from Course 1 Week 3

---
+++ C1W3E1.py
@@ -0,0 +1,11 @@
+# Задание по программированию: Реализация простого класса для чтения из файла
+class FileReader:
+    def __init__(self, path):
+        self.path = path
+
+    def read(self):
+        try:
+            with open(self.path, "r") as f:
+                return f.read()
+        except FileNotFoundError:
+            return ""
