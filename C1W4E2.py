d2be282972de2e3143fc4cc7031e40598fbe798c
FiliVanDev <FiliVanDev@gmail.com>
2020-11-06 14:06:46 +1200

    Add Exercise 2 from Course 1 Week 4

---
+++ C1W4E2.py
@@ -0,0 +1,17 @@
+# Задание по программированию: Дескриптор с комиссией
+class Value:
+    def __init__(self):
+        self.value = None
+
+    def __get__(self, obj, obj_type):
+        return self.value
+
+    def __set__(self, obj, value):
+        self.value = value * (1 - obj.commission)
+
+
+class Account:
+    amount = Value()
+
+    def __init__(self, commission):
+        self.commission = commission
